09c5e4af5316f2300baa16143e65a354018b30c6
Himanshu <pandeyhimanshu2198@gmail.com>
2024-08-01 12:48:43 +0530

    local auto update

--- User_Management_System/obj/Debug/net8.0/User_Management_System.AssemblyInfo.cs
+++ User_Management_System/obj/Debug/net8.0/User_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("User_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0c0d8bdac4272db47165ac448541b2d52a5632")]
 [assembly: System.Reflection.AssemblyProductAttribute("User_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("User_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
