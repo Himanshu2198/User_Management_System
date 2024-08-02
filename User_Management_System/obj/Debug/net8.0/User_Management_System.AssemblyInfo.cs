722a73d5ddb00b648c08087e4a71bfb3118a8c6a
Parth Bajaj <parthbajaj162@gmail.com>
2024-08-02 15:28:20 +0530

    user list entered

--- User_Management_System/obj/Debug/net8.0/User_Management_System.AssemblyInfo.cs
+++ User_Management_System/obj/Debug/net8.0/User_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("User_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c5e4af5316f2300baa16143e65a354018b30c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b12c2015963b734c97afbf845ec8b93a9a60d1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("User_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("User_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
