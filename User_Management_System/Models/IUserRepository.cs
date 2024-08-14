7fd38087b786901d2b1fb84141d8ebbcdd5bbdfa
Himanshu Pandey <himanshu.p@droisys.com>
2024-08-14 16:09:01 +0530

    adding proj structure and nuget packs

---
+++ User_Management_System/Models/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace User_Management_System.Models
+{
+    public interface IUserRepository
+    {
+        void AddUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(User user);
+        void getUserById(string id);
+    }
+}
---
+++ User_Management_System/Models/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+//using System.Configuration;
+
+namespace User_Management_System.Models
+{
+    public class UserRepository:IUserRepository
+    {
+        private SqlConnection con;
+
+
+        private void connection()
+        {
+            //string constr = ConfigurationManager[ConfigurationManager.ConnectionString]["getconn"];
+
+            //con = new SqlConnection(constr);
+        }
+        public void AddUser(User user)
+        {
+            return;
+        }
+
+        public void UpdateUser(User user)
+        {
+            return;
+        }
+        public void DeleteUser(User user) { return; }
+        public void getUserById(string id)
+        {
+            return;
+        }
+    }
+}
