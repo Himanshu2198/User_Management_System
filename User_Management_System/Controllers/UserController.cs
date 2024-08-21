ab2e5ae2d089670a305128d171ee7cc6bdc0fc8d
Himanshu Pandey <himanshu.p@droisys.com>
2024-08-21 11:49:17 +0530

    UserAuth done

--- User_Management_System/Controllers/UserController.cs
+++ User_Management_System/Controllers/UserController.cs
@@ -36,10 +36,20 @@ namespace User_Management_System.Controllers
         }
 
         [HttpPost]
-        public IActionResult RedirectToHome(string userId, string pwd)
+        public IActionResult RedirectToHome(string userName, string pwd)
         {
-            TempData["user"] = userId;
-            return RedirectToAction("Index");
+            SqlDataReader res = newUser.getUserDetails(userName, pwd);
+            if (res.HasRows)
+            {
+                TempData["user"] = userName;
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid user";
+                return View("Login");
+            }
 
         }
         public IActionResult Register()
@@ -48,6 +58,7 @@ namespace User_Management_System.Controllers
         }
 
         public User myUser; 
+        UserRepository newUser = new UserRepository();
         [HttpPost]
         public IActionResult RedirectToLogin(string username,string email,string password,DateOnly dob,string Gender, string Department,string phone)
         {
@@ -61,7 +72,7 @@ namespace User_Management_System.Controllers
             myUser.Phone = phone;
 
             //TempData["newuser"] = myUser;
-            AddUser();
+            newUser.AddUser(myUser);
             return RedirectToAction("Login");
         }
 
@@ -80,41 +91,7 @@ namespace User_Management_System.Controllers
         {
             return View();
         }
-
-
-        /*private SqlConnection con;
-
-
-        private void connection()
-        {
-            string c = Directory.GetCurrentDirectory();
-            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(c).AddJsonFile("appsettings.json").Build();
-            string connectionString = configuration.GetConnectionString("getconn");
-
-            con = new SqlConnection(connectionString);
-
-        }*/
-        public void AddUser()
-        {
-            SqlConnection con = new SqlConnection(@"Data Source=192.168.0.89;Initial Catalog=Userdb;User ID=sa;password=droisys@4800");
-            SqlCommand cmd = new SqlCommand("InsertUser", con);
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("userName", myUser.UserName);
-            cmd.Parameters.AddWithValue("email", myUser.Email);
-            cmd.Parameters.AddWithValue("pass", myUser.Password);
-            cmd.Parameters.AddWithValue("DOB", myUser.DOB);
-            cmd.Parameters.AddWithValue("gen", myUser.Gender);
-            cmd.Parameters.AddWithValue("contact", myUser.Phone);
-            cmd.Parameters.AddWithValue("DeptId", myUser.DeptName );
-            con.Open();
-            int k = cmd.ExecuteNonQuery();
-            if (k != 0)
-            {
-                Console.WriteLine("Record Inserted Succesfully into the Database");
-            }
-            con.Close();
-            return;
-        }
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
