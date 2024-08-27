35258c1a226bca50072b2e270defc672c69f3c72
Aanchal Bisht <aanchal.b@droisys.com>
2024-08-27 15:24:27 +0530

    ..

--- User_Management_System/obj/Debug/net8.0/User_Management_System.AssemblyInfo.cs
+++ User_Management_System/obj/Debug/net8.0/User_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("User_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b09040c61384416caa71f1ea87b123618d2bd739")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2190adaa9eca0bc70414966d7ce4d1771a2dafbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("User_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("User_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
